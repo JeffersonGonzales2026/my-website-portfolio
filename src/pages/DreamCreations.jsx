eddeef5174ba68c69471af78ca43987e1bd6f70d
Jefferson Gonzales <jeffersonguzmangonzales03@gmail.com>
2026-07-30 13:47:34 +0800

    Wired full CMS to Supabase

--- src/pages/DreamCreations.jsx
+++ src/pages/DreamCreations.jsx
@@ -275,7 +275,7 @@ export default function DreamCreations() {
       id: "temp-memorialize-client",
       client_name: "Memorialize Client",
       company: "Memorialize",
-      project_type: "Water",
+      project_type: "Watercolor Portrait",
       rating: 5,
       feedback: "Oh my goodness!!!! Im extremely happy with how it turned out!! Thank you so much… Im sure there will be lots of tears when my Mom opens this!!!",
       face_image_url: "/images/MClient1.png"
